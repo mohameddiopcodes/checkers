ca9a71c620924f0525c8ff036d19df69b7d376b4
Mohamed Diop <yxngbizzy@gmail.com>
2021-09-30 20:01:28 -0700

    fixed bugs

--- js/helpers/animateBoard.js
+++ js/helpers/animateBoard.js
@@ -9,11 +9,10 @@ const someToEat = require('./someToEat')
 const isKing = require('./isKing')
 
 const animateBoard = function(e) {
-    const cellsArr = [...cells]  
+    const cellsArr = [...cells]
+    const capture = e.target.src? someToEat(cellsArr, flatBoard, [e.target.parentElement], turn, isKing([e.target])): false
     
-    if(!highLighted.player && isChip(e) && !highLighted.playerHighLights.length) {
-        const capture = e.target.src? someToEat(cellsArr, flatBoard, [e.target.parentElement], turn, isKing([e.target])): false  
-
+    if(turn === 1 && !highLighted.player && isChip(e) && isWhitePiece(e.target) && !highLighted.playerHighLights.length) {
         highLighted.capture = capture
 
         if(capture) {
@@ -22,6 +21,16 @@ const animateBoard = function(e) {
             utilities({e, highLighted, message}, {displayMessage: 'Make a move', setHighlight: true, flipPlayer: true, pushPlayer: true})
         }
         
+        
+    } else if(turn === -1 && !highLighted.player && isChip(e) && !isWhitePiece(e.target) && !highLighted.playerHighLights.length) {
+        highLighted.capture = capture
+
+        if(capture) {
+            utilities({e, highLighted, message}, {displayMessage: 'Capture your opponent', setHighlight: true, flipPlayer: true, pushPlayer: true})   
+        } else {
+            utilities({e, highLighted, message}, {displayMessage: 'Make a move', setHighlight: true, flipPlayer: true, pushPlayer: true})
+        }
+
     } else if (highLighted.player && !highLighted.cell && !isChip(e) && !highLighted.cellHighLights.length) {
 
         utilities({e, highLighted, message}, {displayMessage: 'Make a move', flipCell: true, pushCell: true})
