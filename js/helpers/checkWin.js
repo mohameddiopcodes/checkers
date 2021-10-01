60cbc704555aeafb87f57b1b475866308bd81e7e
Mohamed Diop <yxngbizzy@gmail.com>
2021-09-30 19:45:03 -0700

    removing unnecessary console logs

--- js/helpers/checkWin.js
+++ js/helpers/checkWin.js
@@ -5,13 +5,10 @@ const checkWin = () => {
     let remaining = flatBoard.filter(item => item === 1 || item === -1)
 
     if(!flatBoard.includes(1)) {
-        console.log(flatBoard)
         utilities({message}, {displayMessage: 'Black Wins', endGame: true})
     } else if(!flatBoard.includes(-1)) {
-        console.log(flatBoard)
         utilities({message}, {displayMessage: 'White Wins', endGame: true})
     } else if(remaining.length === 2) {
-        console.log(flatBoard)
         utilities({message}, {displayMessage: 'Draw', endGame: true})
     }
 }
